2d6ab9a35438af28153175e12bdd06895bb59495
Martin <107601706+Tincho44@users.noreply.github.com>
2023-07-04 18:40:50 -0300

    Update SimuladorAscensores.java

--- src/entregable/SimuladorAscensores.java
+++ src/entregable/SimuladorAscensores.java
@@ -2,11 +2,10 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
@@ -20,6 +19,17 @@ class Ascensor implements Runnable {
     private List<Integer> lugaresDeDetencion;
     private int pisoObjetivo;
     private Semaphore semaforo;
+    private int contador;
+
+    public void agregarPisoDeDetencion(int piso) {
+        if (!lugaresDeDetencion.contains(piso)) {
+            lugaresDeDetencion.add(piso);
+        }
+    }
+
+    public int getContador() {
+        return contador;
+    }
 
     public Ascensor(int id, BlockingQueue<Solicitud> solicitudes, List<Integer> lugaresDeDetencion, Semaphore semaforo) {
         this.id = id;
@@ -28,6 +38,8 @@ class Ascensor implements Runnable {
         this.lugaresDeDetencion = lugaresDeDetencion;
         this.pisoObjetivo = 0;
         this.semaforo = semaforo;
+        this.contador = 0;
+
     }
 
     public void run() {
@@ -40,9 +52,11 @@ class Ascensor implements Runnable {
                  
         if (lugaresDeDetencion.contains(pisoActual)) {
             System.out.println("Ascensor " + id + " Cumplio con una solicitud de paso en en piso" + pisoActual);
+            contador++;
         } 
-                System.out.println("Ascensor " + id + " está en el piso " + pisoActual);
+                System.out.println("Ascensor " + id + " sale del piso " + pisoActual);
                 moverAscensor(pisoDestino);
+                contador++;
                 System.out.println("Ascensor " + id + " ha llegado al piso " + pisoActual);
             }
         } catch (InterruptedException e) {
@@ -81,10 +95,12 @@ class Ascensor implements Runnable {
 }
 
 class AsignadorSolicitudes implements Runnable {
+
     private Semaphore semaforo;
+    private int ultimoAscensorAsignado = 0;
+
     private List<BlockingQueue<Solicitud>> colasAscensores;
     private Queue<Solicitud> solicitudesPendientes;
-    private int tiempoActual;
     private Map<Integer, Ascensor> mapaAscensores;
 
     public AsignadorSolicitudes(Queue<Solicitud> solicitudesPendientes, List<BlockingQueue<Solicitud>> colasAscensores, Map<Integer, Ascensor> mapaAscensores, Semaphore semaforo) {
@@ -97,12 +113,11 @@ class AsignadorSolicitudes implements Runnable {
     public void run() {
         try {
             while (!Thread.currentThread().isInterrupted()) {
-                while (!solicitudesPendientes.isEmpty() && solicitudesPendientes.peek().getTiempo() <= tiempoActual) {
+                while (!solicitudesPendientes.isEmpty()) {
                     Solicitud solicitud = solicitudesPendientes.poll();
                     int ascensorId = asignarAscensor(solicitud.getPisoDestino());
                     colasAscensores.get(ascensorId).put(solicitud);
                 }
-                tiempoActual++;
                 Thread.sleep(1000);
             }
         } catch (InterruptedException e) {
@@ -110,88 +125,103 @@ class AsignadorSolicitudes implements Runnable {
         }
     }
 
-            public int asignarAscensor(int pisoDestino) {
-            // Lógica de asignación personalizada
-            // Aquí puedes implementar tu lógica para determinar a qué ascensor asignar la solicitud
-            // En este ejemplo, se asigna de manera aleatoria
-            List<Ascensor> ascensoresDisponibles = new ArrayList<>();
-            for (Map.Entry<Integer, Ascensor> entry : mapaAscensores.entrySet()) {
-                Ascensor ascensor = entry.getValue();
-                if (ascensor.getPisoActual() <= pisoDestino && ascensor.getPisoObjetivo() >= pisoDestino) {
-                    ascensoresDisponibles.add(ascensor);
-                }
-            }
-            if (!ascensoresDisponibles.isEmpty()) {
-                ascensoresDisponibles.sort(Comparator.comparingInt(Ascensor::getPisoActual));
-                Ascensor ascensorSeleccionado = ascensoresDisponibles.get(0);
-                ascensorSeleccionado.setPisoObjetivo(pisoDestino);
-                return ascensorSeleccionado.getId();
-            } else {
-                int ascensorId = (int) (Math.random() * colasAscensores.size());
-                Ascensor ascensor = mapaAscensores.getOrDefault(ascensorId, null);
-                if (ascensor == null) {
-                    ascensor = new Ascensor(ascensorId + 1, colasAscensores.get(ascensorId), new ArrayList<>(), semaforo);
-                    mapaAscensores.put(ascensorId, ascensor);
-                }
-                ascensor.setPisoObjetivo(pisoDestino);
-                return ascensorId;
-            }
+    public int asignarAscensor(int pisoDestino) {
+        // Se utiliza la variable últimoAscensorAsignado para determinar el siguiente ascensor a asignar
+        int ascensorId = ultimoAscensorAsignado % colasAscensores.size();
+        
+        // Se obtiene el ascensor correspondiente del mapa
+        Ascensor ascensor = mapaAscensores.get(ascensorId);
+    
+        // Se asigna el piso objetivo al ascensor
+        ascensor.setPisoObjetivo(pisoDestino);
+
+        ascensor.agregarPisoDeDetencion(pisoDestino);
+    
+        // Se incrementa el contador para la próxima asignación
+        ultimoAscensorAsignado++;
+    
+        // Se retorna el ID del ascensor asignado
+        return ascensorId;
+    }
+    
+    }
+
+    class Solicitud {
+        private int pisoDestino;
+
+        public Solicitud(int pisoDestino) {
+            this.pisoDestino = pisoDestino;
         }
-    }
 
-class Solicitud {
-    private int pisoDestino;
-    private int tiempo;
-
-    public Solicitud(int pisoDestino, int tiempo) {
-        this.pisoDestino = pisoDestino;
-        this.tiempo = tiempo;
-    }
-
-    public int getPisoDestino() {
-        return pisoDestino;
-    }
-
-    public int getTiempo() {
-        return tiempo;
-    }
-}
-
-public class SimuladorAscensores {
-    public static void main(String[] args) {
-        int numAscensores = 4;
-        List<BlockingQueue<Solicitud>> colasAscensores = new ArrayList<>();
-        Queue<Solicitud> solicitudesPendientes = new PriorityQueue<>(Comparator.comparingInt(Solicitud::getTiempo));
-        Thread[] ascensores = new Thread[numAscensores];
-        Semaphore semaforo = new Semaphore(1); // solo un ascensor a la vez puede tomar una solicitud
-        Map<Integer, Ascensor> mapaAscensores = new HashMap<>();
-
-        for (int i = 0; i < numAscensores; i++) {
-            colasAscensores.add(new ArrayBlockingQueue<>(11)); // al poner capacidad 11, llega hasta el piso 10, dado que cuenta a 0 para el indice
-            List<Integer> lugaresDeDetencion = new ArrayList<>();
-            Ascensor ascensor = new Ascensor(i + 1, colasAscensores.get(i), lugaresDeDetencion, semaforo);
-            mapaAscensores.put(i, ascensor);
-            ascensores[i] = new Thread(ascensor);
-            ascensores[i].start();
+        public int getPisoDestino() {
+            return pisoDestino;
         }
+
+    }
+
+    public class SimuladorAscensores {
+        public static void main(String[] args) {
+            int numAscensores = 4;
+            List<BlockingQueue<Solicitud>> colasAscensores = new ArrayList<>();
+            Queue<Solicitud> solicitudesPendientes = new LinkedList<>(); // Cambiado de PriorityQueue a LinkedList
+            Thread[] ascensores = new Thread[numAscensores];
+            Semaphore semaforo = new Semaphore(1); // solo un ascensor a la vez puede tomar una solicitud
+            Map<Integer, Ascensor> mapaAscensores = new HashMap<>();
+
+            for (int i = 0; i < numAscensores; i++) {
+                colasAscensores.add(new ArrayBlockingQueue<>(21)); // al poner capacidad 11, llega hasta el piso 10, dado que cuenta a 0 para el indice
+                List<Integer> lugaresDeDetencion = new ArrayList<>();
+                Ascensor ascensor = new Ascensor(i + 1, colasAscensores.get(i), lugaresDeDetencion, semaforo);
+                mapaAscensores.put(i, ascensor);
+                ascensores[i] = new Thread(ascensor);
+                ascensores[i].start();
+            }
+
 
         AsignadorSolicitudes asignadorSolicitudes = new AsignadorSolicitudes(solicitudesPendientes, colasAscensores, mapaAscensores, semaforo);
         Thread hiloAsignador = new Thread(asignadorSolicitudes);
         hiloAsignador.start();
 
-        String archivo = "instrucciones.txt";
+        String archivo = "/Users/tincho/Desktop/sistemasOperativos-ascensor-main/src/entregable/instrucciones.txt";
 
         try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
             String linea;
             while ((linea = br.readLine()) != null) {
-                String[] partes = linea.split(",");
-                int pisoDestino = Integer.parseInt(partes[0]);
-                int tiempo = Integer.parseInt(partes[1]);
-                Solicitud solicitud = new Solicitud(pisoDestino, tiempo);
+                int pisoDestino = Integer.parseInt(linea);
+                Solicitud solicitud = new Solicitud(pisoDestino);
                 solicitudesPendientes.offer(solicitud);
             }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+
+        try {
+            Thread.sleep(30000); // ajusta este valor segun sea necesario, cuantas mas solicitudes haya, mas tiempo debe esperar
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        
+        // Ahora interrumpe los hilos de los ascensores
+        for (Thread ascensor : ascensores) {
+            ascensor.interrupt();
+        }
+        
+        // Espera a que todos los hilos terminen antes de imprimir las estadísticas
+        for (Thread ascensor : ascensores) {
+            try {
+                ascensor.join();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }        
+
+        System.out.println(); // Esto imprime un salto de línea
+        System.out.println("Estadisticas de Ascensores: ");
+
+        // Imprimir el contador de cada ascensor
+        for (Map.Entry<Integer, Ascensor> entry : mapaAscensores.entrySet()) {
+            Ascensor ascensor = entry.getValue();
+            System.out.println("El ascensor " + ascensor.getId() + " ha procesado " + ascensor.getContador() + " solicitudes.");
         }
     }
 }
